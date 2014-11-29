3a9401d1d5514f0eab78cc17fd77dc62109726ee
rapospectre <rapospectre@0163.com>
2014-11-29 16:41:32 +0800

    update

--- HomeApi/admin.py
+++ HomeApi/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from HomeApi.models import *
 # Register your models here.
+
+admin.site.register(HomeAdmin)
+admin.site.register(HomeItem_P)
+admin.site.register(HomeItem_O)
+admin.site.register(HomeItem)
+admin.site.register(Advertisment)
+admin.site.register(Appointment)
+admin.site.register(Block)
+admin.site.register(Notice)
+admin.site.register(PhoneVerify)
+admin.site.register(Consumer)
