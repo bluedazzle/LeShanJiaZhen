263acbebce2ea64d9a98364ce11bf42908ef3fbc
octoplus <932499838@qq.com>
2014-12-01 13:20:30 +0800

    update

--- HomeApi/views.py
+++ HomeApi/views.py
@@ -140,8 +140,8 @@ def add_homeitem(request):
         status = 6
     except NoneExistError:
         status = 7
-    # except Exception:
-    #     status = 2
+    except Exception:
+        status = 2
     return HttpResponse(json.dumps({'status': status, 'body': 'null'}))
 
 
@@ -179,8 +179,78 @@ def change_homeitem(request):
         status = 1
     except NoneExistError:
         status = 7
-    # except Exception:
-    #     status = 2
+    except Exception:
+        status = 2
     return HttpResponse(json.dumps({'status': status, 'body': 'null'}))
 
 
+@csrf_exempt
+def add_block(request):
+    try:
+        req = json.loads(request.body)
+        area_name = req['area_name']
+        area_tel = int(req['area_tel'])
+        area_info = req['area_info']
+        area_admin = req['area_admin']
+        if len(Block.objects.filter(area_name=area_name)) != 0:
+            raise AlreadyExitError
+        if len(HomeAdmin.objects.filter(username=area_admin)) == 0:
+            raise NoneExistError
+        p = Block(area_name=area_name, area_tel=area_tel, area_info=area_info)
+        p_admin = HomeAdmin.objects.get(username=area_admin)
+        p.area_admin = p_admin
+        p.save()
+        status = 1
+    except AlreadyExitError:
+        status = 6
+    except NoneExistError:
+        status = 7
+    except Exception:
+        status = 2
+    return HttpResponse(json.dumps({"status": status, "body": 'null'}))
+
+
+
+@csrf_exempt
+def del_block(request):
+    try:
+        req = json.loads(request.body)
+        area_name = req['area_name']
+        if len(Block.objects.filter(area_name=area_name)) == 0:
+            raise NoneExistError
+        p = Block.objects.get(area_name=area_name)
+        p.delete()
+        status = 1
+    except NoneExistError:
+        status = 7
+    except Exception:
+        status = 2
+    return HttpResponse(json.dumps({"status": status, "body": 'null'}))
+
+
+@csrf_exempt
+def change_block(request):
+    try:
+        req = json.loads(request.body)
+        area_name = req['area_name']
+        new_area_name = req['new_area_name']
+        area_tel = int(req['area_tel'])
+        area_info = req['area_info']
+        area_admin = req['area_admin']
+        if len(Block.objects.filter(area_name=area_name)) == 0:
+            raise NoneExistError
+        if len(HomeAdmin.objects.filter(username=area_admin)) == 0:
+            raise NoneExistError
+        p = Block.objects.get(area_name=area_name)
+        p.area_name = new_area_name
+        p.area_tel = area_tel
+        p.area_info = area_info
+        p_admin = HomeAdmin.objects.get(username=area_admin)
+        p.area_admin = p_admin
+        p.save()
+        status = 1
+    except NoneExistError:
+        status = 7
+    except Exception:
+        status = 2
+    return HttpResponse(json.dumps({"status": status, "body": 'null'}))
