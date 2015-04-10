3b7c1b8b4f906b76cb50dabc504e48c9ce612f3f
rapospectre <rapospectre@163.com>
2015-04-10 18:49:55 +0800

    update

--- HomeApi/management/commands/test.py
+++ HomeApi/management/commands/test.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
 from HomeApi.views import *
+from HomeApi.OnlinePay import *
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        create_order_id()
+        rse = refund_order('ch_8qHyv9Tm5yT0T04abLOSabDO', 'test', 1)
+        print rse
